144851cac0226dab33b2aced0ab66d60c52ded5b
EngineerMansha <manshauaf@gmail.com>
2021-11-24 13:11:12 +0500

    learn unshift & shift

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-// var a=["Ali","Hamza","Waqas"];
-// a.pop() // pop use for remove last element of array
+// // var a=["Ali","Hamza","Waqas"];
+// // a.pop() // pop use for remove last element of array
+// // console.log(a);
+
+
+
+//  var a=["Ali","Hamza","Waqas"];
+// a.push("Talha") //use for add the element last of array by passing the value in push("Value name")
 // console.log(a);
 
-
-
- var a=["Ali","Hamza","Waqas"];
-a.push("Talha") //use for add the element last of array by passing the value in push("Value name")
+var a=["Ali","Hamza","Waqas"];
+a.unshift("talha","abbas")
 console.log(a);
-
- 
