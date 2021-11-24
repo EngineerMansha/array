8f58c8ee1df41986e87bb963a31d72e941555f52
EngineerMansha <manshauaf@gmail.com>
2021-11-24 13:32:33 +0500

    done array

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-// // var a=["Ali","Hamza","Waqas"];
-// // a.pop() // pop use for remove last element of array
+// // // var a=["Ali","Hamza","Waqas"];
+// // // a.pop() // pop use for remove last element of array
+// // // console.log(a);
+
+
+
+// //  var a=["Ali","Hamza","Waqas"];
+// // a.push("Talha") //use for add the element last of array by passing the value in push("Value name")
 // // console.log(a);
 
-
-
-//  var a=["Ali","Hamza","Waqas"];
-// a.push("Talha") //use for add the element last of array by passing the value in push("Value name")
+// // var a=["Ali","Hamza","Waqas"];
+// // a.unshift("talha","abbas")
+// // console.log(a);
+// var a=["Ali","Hamza","Waqas","Rameez"];
+// a.splice(0,1,"Hello")
 // console.log(a);
-
-var a=["Ali","Hamza","Waqas"];
-a.unshift("talha","abbas")
-console.log(a);
