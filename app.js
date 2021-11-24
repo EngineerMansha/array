d02d9c90bf1fab15a95a5ba78a71436dbf24f724
EngineerMansha <manshauaf@gmail.com>
2021-11-24 12:29:28 +0500

    array use

---
+++ app.js
@@ -0,0 +1,11 @@
+// var a=["Ali","Hamza","Waqas"];
+// a.pop() // pop use for remove last element of array
+// console.log(a);
+
+
+
+ var a=["Ali","Hamza","Waqas"];
+a.push("Talha") //use for add the element last of array by passing the value in push("Value name")
+console.log(a);
+
+ 
